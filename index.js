87e262b9f8bf382cfca210db7d91061c03028fb3
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-05 19:53:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function submitData(usersName, usersEmail){
-  
+  let formData = {
+  name: usersName,
+  email: usersEmail
+};
+ 
+let configObj = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
 }
